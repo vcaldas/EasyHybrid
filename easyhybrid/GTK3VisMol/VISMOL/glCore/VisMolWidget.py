Fixed rotation in text rendering, now the letters don't rotate when the view does. The problem was solved taken the coordinates for the letters in absolute coordinates (multiplying the point by the vismol object model matrix) and then taken out the model matrix on the letters.

easyhybrid/GTK3VisMol/VISMOL/glCore/VisMolWidget.py
@@ -1187,17 +1187,17 @@ class VisMolWidget():
     
     def _draw_freetype(self, visObj = None):
         """ Function doc """
-        #GL.glEnable(GL.GL_DEPTH_TEST)
         GL.glDisable(GL.GL_DEPTH_TEST)
         GL.glEnable(GL.GL_BLEND)
         GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
         GL.glUseProgram(self.freetype_program)
-        #print(self.model_mat,'<-- model_mat widget')
-        visObj.vm_font.load_matrices(self.freetype_program, np.copy(visObj.model_mat), self.glcamera.view_matrix, self.glcamera.projection_matrix)
+        visObj.vm_font.load_matrices(self.freetype_program, self.glcamera.view_matrix, self.glcamera.projection_matrix)
         visObj.vm_font.load_font_params(self.freetype_program)
         GL.glBindVertexArray(visObj.vm_font.vao)
         texto = visObj.name
         point = np.array(visObj.mass_center,np.float32)
+        point = np.array((point[0],point[1],point[2],1),np.float32)
+        point = np.dot(point, visObj.model_mat)
         GL.glBindTexture(GL.GL_TEXTURE_2D, visObj.vm_font.texture_id)
         for i,c in enumerate(texto):
             c_id = ord(c)
@@ -1212,7 +1212,6 @@ class VisMolWidget():
             GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
             GL.glDrawArrays(GL.GL_POINTS, 0, 1)
         GL.glDisable(GL.GL_BLEND)
-        #GL.glDisable(GL.GL_DEPTH_TEST)
         GL.glBindVertexArray(0)
         GL.glUseProgram(0)
     

easyhybrid/GTK3VisMol/VISMOL/glCore/vismol_font.py
@@ -116,12 +116,8 @@ class VisMolFont():
         self.vbos = (coord_vbo, tex_vbo)
         return True
     
-    def load_matrices(self, program, model_mat, view_mat, proj_mat):
+    def load_matrices(self, program, view_mat, proj_mat):
         """ Function doc """
-        #model_mat[:3,:3] = np.identity(3)
-        #print(model_mat,'<- model_mat text')
-        model = GL.glGetUniformLocation(program, 'model_mat')
-        GL.glUniformMatrix4fv(model, 1, GL.GL_FALSE, model_mat)
         view = GL.glGetUniformLocation(program, 'view_mat')
         GL.glUniformMatrix4fv(view, 1, GL.GL_FALSE, view_mat)
         proj = GL.glGetUniformLocation(program, 'proj_mat')